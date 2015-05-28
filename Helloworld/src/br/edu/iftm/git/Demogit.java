5de17a4dfd9314f9f75dac72ba647cf7933ecd5b
Wildes Oliveira Batista <wildes@autcla.com.br>
2015-05-28 20:09:41 -0300

    Commit inicial do projeto

---
+++ Helloworld/src/br/edu/iftm/git/Demogit.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.git;
+
+public class Demogit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("Veja o git no eclipse");
+
+	}
+
+}
